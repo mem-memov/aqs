47c1ca788c1ab584efc630167d0f67dd5fa4c9e0
Alexander Novikov <mem-memov@yandex.ru>
2019-06-10 22:44:06 +0300

    hello world

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import scala.util.{Failure, Success}
+import scala.concurrent.Await
 
 object Main extends App {
 
@@ -8,4 +11,23 @@ object Main extends App {
 
   implicit val system: ActorSystem = ActorSystem(name = "todoapi")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  import system.dispatcher
+  import akka.http.scaladsl.server.Directives._
+
+  def route = path("hello") {
+    get {
+      complete("Hello, World!")
+    }
+  }
+
+  val binding = Http().bindAndHandle(route, host, port)
+
+  binding.onComplete {
+    case Success(_) => println("Success!")
+    case Failure(error) => println(s"Faled: " +  error.getMessage)
+  }
+
+  import scala.concurrent.duration._
+  Await.result(binding, 3.seconds)
 }
